826701d89ff7c795cde5ea022d3a5ca5ee87d923
reggie <reggierandhawa@gmail.com>
2018-03-09 11:28:21 -0800

    diwali ecard

---
+++ index.js
@@ -0,0 +1,39 @@
+var factbox = document.getElementById("factbox");
+
+document.getElementById("button1").addEventListener("click", function() {
+    factbox.style.backgroundImage = "url(svgs/fact1.svg)";
+});
+
+document.getElementById("button2").addEventListener("click", function() {
+    factbox.style.backgroundImage = "url(svgs/fact2.svg)";
+});
+
+document.getElementById("button3").addEventListener("click", function() {
+    factbox.style.backgroundImage = "url(svgs/fact3.svg)";
+});
+
+document.getElementById("button4").addEventListener("click", function() {
+    factbox.style.backgroundImage = "url(svgs/fact4.svg)";
+});
+
+document.getElementById("button5").addEventListener("click", function() {
+    factbox.style.backgroundImage = "url(svgs/fact5.svg)";
+});
+
+document.getElementById("button6").addEventListener("click", function() {
+    factbox.style.backgroundImage = "url(svgs/fact6.svg)";
+});
+document.getElementById("button7").addEventListener("click", function() {
+    factbox.style.backgroundImage = "url(svgs/fact7.svg)";
+});
+document.getElementById("button8").addEventListener("click", function() {
+    factbox.style.backgroundImage = "url(svgs/fact8.svg)";
+});
+
+document.getElementById("button9").addEventListener("click", function() {
+    factbox.style.backgroundImage = "url(svgs/fact9.svg)";
+});
+
+document.getElementById("button10").addEventListener("click", function() {
+    factbox.style.backgroundImage = "url(svgs/fact10.svg)";
+});
